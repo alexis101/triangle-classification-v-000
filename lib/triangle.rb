ac7b728ec0c5c530157552f333d8c01eca0ced54
alexis101 <kondilisalexis@gmail.com>
2018-06-20 00:28:56 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,29 +1,26 @@
 class Triangle
   # write code here
-attr_accessor :side_1, :side_2, :side_3
-
-  def initialization(side_1, side_2, side_3)
-    @side_1 = side_1
-   @side_2 = side_2
-   @side_3 = side_3
-   @sides_array = [@side_1, @side_2, @side_3]
+  attr_accessor :lengths
+  def initialize(l1, l2, l3)
+    @lengths = [l1, l2, l3]
   end
-  def kind
-      if lengths.any? {|l| l <= 0 } || (lengths[1] + lengths[2] <= lengths[0] || lengths[0] + lengths[2] <= lengths[1] || lengths[0] + lengths[1] <= lengths[2])
-        raise TriangleError
-      elsif lengths.all? {|l| l == lengths[0]}
-        :equilateral
-      elsif lengths[1] == lengths[2] || lengths[0] == lengths[2] || lengths[0] == lengths[1]
-        :isosceles
-      elsif lengths.all? {|l| lengths.reject {|r| l == r}.all?{|v| v != l}}
-        :scalene
-      end
 
+  def kind
+    if lengths.any? {|l| l <= 0 } || (lengths[1] + lengths[2] <= lengths[0] || lengths[0] + lengths[2] <= lengths[1] || lengths[0] + lengths[1] <= lengths[2])
+      raise TriangleError
+    elsif lengths.all? {|l| l == lengths[0]}
+      :equilateral
+    elsif lengths[1] == lengths[2] || lengths[0] == lengths[2] || lengths[0] == lengths[1]
+      :isosceles
+    elsif lengths.all? {|l| lengths.reject {|r| l == r}.all?{|v| v != l}}
+      :scalene
     end
 
-
   end
 
-  class TriangleError < StandardError
 
-  end
+end
+
+class TriangleError < StandardError
+
+end
